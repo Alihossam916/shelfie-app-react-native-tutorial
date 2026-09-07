1db15f4e033f88cd4ab0254f6a44eab3816f919b
Alihossam916 <alihossam916@gmail.com>
2026-09-07 21:50:05 +0300

    feat: apply color theming to About and RootLayout components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, useColorScheme } from "react-native";
 import { Stack } from "expo-router";
+import { Colors } from "../constants/colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme ?? "light"];
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "#ddd" },
-        headerTintColor: "#333",
-      }}
-    >
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
-      <Stack.Screen
-        name="contact"
-        options={{ title: "Contact", headerShown: false }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="auto" />
+
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen name="contact" options={{ title: "Contact" }} />
+      </Stack>
+    </>
   );
 };
 
